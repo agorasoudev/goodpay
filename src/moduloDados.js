f5dd534af9dfed64cb02bc238d01b6321abfa906
Jaqueline Rodrigues <allanisdeniane@gmail.com>
2021-12-20 18:04:57 -0300

    Adição de módulos e modais

---
+++ src/moduloDados.js
@@ -0,0 +1,7 @@
+
+const userLogin = [
+    {id: 1, username:'victoricoma', senha:'gordinho123', email:'victoricoma@soulcodeacademy.org'},
+    {id: 2, username:'marcelofarofa', senha:'ff123456', email:'marceloff@gmail.com'}
+]
+
+export default userLogin
